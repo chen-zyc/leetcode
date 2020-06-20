ba939397b24320ab406675f683442c386fbf8e95
zhangyuchen <zhangyuchen0411@163.com>
2020-06-20 11:43:28 +0800

    再做一次：10-正则表达式匹配。

--- impl-rust/src/no_0010_regular_expression_matching.rs
+++ impl-rust/src/no_0010_regular_expression_matching.rs
@@ -51,7 +51,7 @@ impl Solution {
         let s = s.as_bytes();
         let p = p.as_bytes();
         let (slen, plen) = (s.len(), p.len());
-        let mut memo = vec![vec![None; plen + 1]; slen + 1];
+        let mut memo = vec![vec![None; plen + 1]; slen + 1]; // 这里不需要用 Option, 见 is_match4
         memo[slen][plen] = Some(true);
 
         for i in (0..=slen).rev() {
@@ -65,10 +65,34 @@ impl Solution {
                 } else {
                     memo[i][j] = Some(first_match && memo[i + 1][j + 1] == Some(true));
                 }
+                // println!("m[{}][{}] = {:?}", i, j, memo[i][j]);
             }
         }
 
         memo[0][0] == Some(true)
+    }
+
+    // 2020-06-20 再做一次，独立完成。
+    pub fn is_match4(s: String, p: String) -> bool {
+        let s = s.as_bytes();
+        let p = p.as_bytes();
+
+        let mut memo = vec![vec![false; p.len() + 1]; s.len() + 1];
+        memo[s.len()][p.len()] = true;
+
+        for i in (0..=s.len()).rev() {
+            for j in (0..p.len()).rev() {
+                // 现在要计算 memo[i][j]
+                let first_match = i < s.len() && (s[i] == p[j] || (p[j] == '.' as u8));
+                if j + 1 < p.len() && p[j + 1] == '*' as u8 {
+                    memo[i][j] = memo[i][j + 2] || (first_match && memo[i + 1][j]);
+                } else {
+                    memo[i][j] = first_match && memo[i + 1][j + 1];
+                }
+                // println!("m[{}][{}] = {}", i, j, memo[i][j]);
+            }
+        }
+        memo[0][0]
     }
 }
 
@@ -121,5 +145,11 @@ mod test {
             Solution::is_match3("ab".to_owned(), ".*c".to_owned()),
             false
         );
+    }
+
+    #[test]
+    fn test_is_match4() {
+        assert_eq!(Solution::is_match4("ab".to_owned(), ".*".to_owned()), true);
+        // assert_eq!(Solution::is_match3("ab".to_owned(), ".*".to_owned()), true);
     }
 }
